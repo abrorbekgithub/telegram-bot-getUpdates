2b11be9bf52ba9a21a62a037258526816fadfd77
abrorbek <abrorbekgithub@gmail.com>
2021-01-31 16:58:35 +0500

    telegram bot firststep_bot  getUpdates

---
+++ getUpdates.py
@@ -0,0 +1,10 @@
+import requests
+import json
+from pprint import pprint
+
+token='1690220485:AAHXCPppQIBAypOyRADaSmp3UHsH2UfOhM4'
+url=f'https://api.telegram.org/bot{token}/getMe'
+r=requests.get(url=url)
+data=r.json()
+updates=data["result"]
+pprint(updates)
