74302188b516cfbb6c7bcd5fd7930b32830ca1aa
abrorbek <abrorbekgithub@gmail.com>
2021-02-04 10:37:19 +0500

    changing token

--- sendMessages.py
+++ sendMessages.py
@@ -2,12 +2,14 @@ import requests
 import json
 from pprint import pprint
 
-token='1690220485:AAHXCPppQIBAypOyRADaSmp3UHsH2UfOhM4'
-url2=f'https://api.telegram.org/bot{token}/sendMessage'
-
-p={
+def sendMessage():
+    url=f'https://api.telegram.org/bot{token}/sendMessage'
+    p={
         "chat_id":1091584118,                
         "text": "salom"                             
-}
+    }
+    res=requests.get(url=url,params=p)
 
-res=requests.get(url=url2,params=p)
+token='1690220485:AAHXCPppQIBAypOyRADaSmp3UHsH2UfOhM4'
+
+sendMessage()
